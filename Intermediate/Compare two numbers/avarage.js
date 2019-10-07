73ad1c6c5e6bb5fccf926efcddc300e204917b96
Haris <learn@learntpoint.com>
2019-10-07 10:32:13 +0200

    Website Challenge

--- Intermediate/Compare two numbers/avarage.js
+++ Intermediate/Compare two numbers/avarage.js
@@ -1,8 +1,4 @@
-var "Martin" = 76;
-var "Thimas" = 85;
-var "Klaus" =65;
-var "Maria" = 93;
-var "David" 81;
+
 
 if(numbergrade <= 60){
 	document.getElementById('letter').value = "F";
@@ -10,6 +6,16 @@ if(numbergrade <= 60){
 else if(61 <= numbergrade && numbergrade <= 70){ // you reach this point only if all previous confitions are false
 	document.getElementById('letter').value = "D";
 }
-else if(71 <= numbergrade && numbergrade >= 80){ // you reach this point only if all previous confitions are false
-	document.getElementById('letter').value = "A-";
+else if(71 <= numbergrade && numbergrade <= 80){ // you reach this point only if all previous confitions are false
+	document.getElementById('letter').value = "C";
 }
+else if(81 <= numbergrade && numbergrade <= 90){ // you reach this point only if all previous confitions are false
+	document.getElementById('letter').value = "B";
+}
+else if(91 <= numbergrade && numbergrade <= 100){ // you reach this point only if all previous confitions are false
+	document.getElementById('letter').value = "A";
+}
+
+
+
+console.log(letter);
